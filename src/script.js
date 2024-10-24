aa987b4dbb2652f3b5ea693e7eb16937f5e5d084
Alper Efe Şahin <efe.mate@gmail.com>
2024-10-25 00:47:10 +0300

    change variable names

--- src/script.js
+++ src/script.js
@@ -1,20 +1,20 @@
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-async function handleOutput(type, data) {
-  const styles = `padding: 0.5rem 0; font-size: 1rem; font-weight: 700;`;
+async function handleOutput(outputType, outputData) {
+  const outputStyles = `padding: 0.5rem 0; font-size: 1rem; font-weight: 700;`;
 
-  if (type === "PROGRESS") {
+  if (outputType === "PROGRESS") {
     console.clear();
-    console.warn(`%cProgress: ${data.currentPageCount}/${data.followingCount} (${parseInt(data.currentPageCount / data.followingCount * 100)}%)`, styles);
-  } else if (type === "FINISH") {
+    console.warn(`%cProgress: ${outputData.currentPageCount}/${outputData.totalFollowing} (${parseInt(outputData.currentPageCount / outputData.totalFollowing * 100)}%)`, outputStyles);
+  } else if (outputType === "FINISH") {
     console.clear();
-    if (data.unfollowers.length === 0) {
-      return console.warn(`%cPROCESS FINISHED - Everyone followed you back! 😄`, styles);
+    if (outputData.unfollowers.length === 0) {
+      return console.warn(`%cPROCESS FINISHED - Everyone followed you back! 😄`, outputStyles);
     }
 
-    console.group(`%cPROCESS FINISHED - ${data.unfollowers.length} ${data.unfollowers.length === 1 ? "user" : "users"} didn't follow you back. 🤬 (Verified users not included)`, styles);
+    console.group(`%cPROCESS FINISHED - ${outputData.unfollowers.length} ${outputData.unfollowers.length === 1 ? "user" : "users"} didn't follow you back. 🤬 (Verified users not included)`, outputStyles);
 
-    data.unfollowers.forEach(unfollower => {
+    outputData.unfollowers.forEach(unfollower => {
       console.log(`%c@%c${unfollower.username}`, "color: blue;", "color: black;");
     });
 
@@ -23,23 +23,23 @@ async function handleOutput(type, data) {
   }
 }
 
-class Script {
+class UnfollowerChecker {
   constructor() {
     this.unfollowers = [];
-    this.canQuery = false;
-    this.nextPageHash = "";
-    this.requestsCount = 0;
-    this.followingCount = 0;
+    this.hasMorePages = false;
+    this.nextPageCursor = "";
+    this.requestCount = 0;
+    this.totalFollowing = 0;
     this.currentPageCount = 0;
-    this.estimatedStepValue = 0;
+    this.estimatedStepSize = 0;
   }
 
   getCookie(cookieName) {
     return new Promise((resolve, reject) => {
       const cookies = document.cookie.split(";");
       for (const cookie of cookies) {
-        const pair = cookie.split("=");
-        if (pair[0].trim() === cookieName) resolve(decodeURIComponent(pair[1]));
+        const [name, value] = cookie.split("=");
+        if (name.trim() === cookieName) resolve(decodeURIComponent(value));
       }
       reject("Cookie not found!");
     });
@@ -59,7 +59,7 @@ class Script {
         first: "1000"
       }
     };
-    if (this.nextPageHash) params.variables.after = this.nextPageHash;
+    if (this.nextPageCursor) params.variables.after = this.nextPageCursor;
     return `https://www.instagram.com/graphql/query/?${this.createURLParamsString(params)}`;
   }
 
@@ -75,17 +75,17 @@ class Script {
           }
         });
 
-        this.canQuery = data.user.edge_follow.page_info.has_next_page;
-        this.nextPageHash = data.user.edge_follow.page_info.end_cursor;
-        this.requestsCount++;
-        this.followingCount = data.user.edge_follow.count;
+        this.hasMorePages = data.user.edge_follow.page_info.has_next_page;
+        this.nextPageCursor = data.user.edge_follow.page_info.end_cursor;
+        this.requestCount++;
+        this.totalFollowing = data.user.edge_follow.count;
         this.currentPageCount += data.user.edge_follow.edges.length;
 
-        if (this.estimatedStepValue === 0) this.estimatedStepValue = data.user.edge_follow.edges.length;
-        handleOutput("PROGRESS", { currentPageCount: this.currentPageCount, followingCount: this.followingCount });
+        if (this.estimatedStepSize === 0) this.estimatedStepSize = data.user.edge_follow.edges.length;
+        handleOutput("PROGRESS", { currentPageCount: this.currentPageCount, totalFollowing: this.totalFollowing });
 
         await sleep(3000);
-      } while (this.canQuery);
+      } while (this.hasMorePages);
 
       handleOutput("FINISH", { unfollowers: this.unfollowers });
     } catch (error) {
@@ -97,5 +97,5 @@ class Script {
   }
 }
 
-const script = new Script();
-script.startScript();
+const unfollowerChecker = new UnfollowerChecker();
+unfollowerChecker.startScript();
