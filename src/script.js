0ad9bb6eef5eed8bd16eb9ea172bce54e4b8805e
Alper Efe Şahin <efe.mate@gmail.com>
2024-10-25 00:54:56 +0300

    update variable names

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ async function handleOutput(outputType, outputData) {
 
   if (outputType === "PROGRESS") {
     console.clear();
-    console.warn(`%cProgress: ${outputData.currentPageCount}/${outputData.totalFollowing} (${parseInt(outputData.currentPageCount / outputData.totalFollowing * 100)}%)`, outputStyles);
+    console.warn(`%cIn progress`, outputStyles);
   } else if (outputType === "FINISH") {
     console.clear();
     if (outputData.unfollowers.length === 0) {
@@ -23,67 +23,64 @@ async function handleOutput(outputType, outputData) {
   }
 }
 
-class UnfollowerChecker {
+class FollowerCheck {
   constructor() {
     this.unfollowers = [];
     this.hasMorePages = false;
-    this.nextPageCursor = "";
-    this.requestCount = 0;
-    this.totalFollowing = 0;
-    this.currentPageCount = 0;
-    this.estimatedStepSize = 0;
+    this.paginationCursor = "";
+    this.apiRequestCount = 0;
+    this.totalFollowings = 0;
+    this.processedFollowings = 0;
   }
 
-  getCookie(cookieName) {
+  getCookieValue(cookieName) {
     return new Promise((resolve, reject) => {
       const cookies = document.cookie.split(";");
       for (const cookie of cookies) {
-        const [name, value] = cookie.split("=");
-        if (name.trim() === cookieName) resolve(decodeURIComponent(value));
+        const pair = cookie.split("=");
+        if (pair[0].trim() === cookieName) resolve(decodeURIComponent(pair[1]));
       }
       reject("Cookie not found!");
     });
   }
 
-  createURLParamsString(params) {
+  createQueryString(params) {
     return Object.keys(params)
       .map(key => `${key}=${typeof params[key] === "object" ? JSON.stringify(params[key]) : params[key]}`)
       .join("&");
   }
 
-  async generateURL() {
-    const params = {
+  async buildApiUrl() {
+    const queryParams = {
       query_hash: "3dec7e2c57367ef3da3d987d89f9dbc8",
       variables: {
-        id: await this.getCookie("ds_user_id"),
+        id: await this.getCookieValue("ds_user_id"),
         first: "1000"
       }
     };
-    if (this.nextPageCursor) params.variables.after = this.nextPageCursor;
-    return `https://www.instagram.com/graphql/query/?${this.createURLParamsString(params)}`;
+    if (this.paginationCursor) queryParams.variables.after = this.paginationCursor;
+    return `https://www.instagram.com/graphql/query/?${this.createQueryString(queryParams)}`;
   }
 
-  async startScript() {
+  async start() {
     try {
       do {
-        const url = await this.generateURL();
-        const { data } = await fetch(url).then(res => res.json());
+        const apiUrl = await this.buildApiUrl();
+        const { data } = await fetch(apiUrl).then(res => res.json());
 
-        data.user.edge_follow.edges.forEach(edge => {
-          if (!edge.node.is_verified && !edge.node.follows_viewer) {
-            this.unfollowers.push({ username: edge.node.username });
+        data.user.edge_follow.edges.forEach(following => {
+          if (!following.node.is_verified && !following.node.follows_viewer) {
+            this.unfollowers.push({ username: following.node.username });
           }
         });
 
         this.hasMorePages = data.user.edge_follow.page_info.has_next_page;
-        this.nextPageCursor = data.user.edge_follow.page_info.end_cursor;
-        this.requestCount++;
-        this.totalFollowing = data.user.edge_follow.count;
-        this.currentPageCount += data.user.edge_follow.edges.length;
+        this.paginationCursor = data.user.edge_follow.page_info.end_cursor;
+        this.apiRequestCount++;
+        this.totalFollowings = data.user.edge_follow.count;
+        this.processedFollowings += data.user.edge_follow.edges.length;
 
-        if (this.estimatedStepSize === 0) this.estimatedStepSize = data.user.edge_follow.edges.length;
-        handleOutput("PROGRESS", { currentPageCount: this.currentPageCount, totalFollowing: this.totalFollowing });
-
+        handleOutput("PROGRESS", {});
         await sleep(3000);
       } while (this.hasMorePages);
 
@@ -92,10 +89,10 @@ class UnfollowerChecker {
       console.error(`Something went wrong!\n${error}`);
       console.warn("Retrying after 15 seconds...");
       await sleep(15000);  // Retry after 15 seconds on any error
-      this.startScript();   // Restart the script
+      this.start();   // Restart the script
     }
   }
 }
 
-const unfollowerChecker = new UnfollowerChecker();
-unfollowerChecker.startScript();
+const followerCheck = new FollowerCheck();
+followerCheck.start();
